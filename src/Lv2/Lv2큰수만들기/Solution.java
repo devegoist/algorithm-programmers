fcde6e6fc0ddd87cc2450c915debca887ed54639
eunsoo88kim <eskim@station3.co.kr>
2019-03-26 14:13:24 +0900

    1000 같은 타입의 입력값도 처리 가능하게 변경
    1000 같은 타입의 입력값도 처리 가능하게 변경

--- src/Lv2/Lv2큰수만들기/Solution.java
+++ src/Lv2/Lv2큰수만들기/Solution.java
@@ -6,40 +6,31 @@ package Lv2.Lv2큰수만들기;
  */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(solution("1924", 0));
+        System.out.println(solution("1924", 1));
     }
 
     public static String solution(String number, int k) {
         int size = number.length() - k;
 
-        return getValue(number, size);
+        return getValue("", number, size);
     }
 
-    private static String getValue(String number, int size) {
+    private static String getValue(String preNumber, String number, int size) {
         int index = 0;
-        String value;
-        String maxValue = "0";
+        String maxNumber = "0";
 
         while (index < number.length()) {
-            value = number.substring(index, index + 1);
+            String currentPreNum = preNumber + number.substring(index, index + 1);
 
-            if (size > 1) {
-                if (size - 1 > number.substring(index + 1).length()) {
-                    index++;
-                    continue;
-                }
-
-                value += getValue(number.substring(index + 1), size - 1);
+            if (size == 1) {
+                maxNumber = String.valueOf(Math.max(Integer.valueOf(maxNumber), Integer.valueOf(currentPreNum)));
             } else {
-                value = number.substring(index, index + 1);
+                maxNumber = String.valueOf(Math.max(Integer.valueOf(maxNumber), Integer.valueOf(getValue(currentPreNum, number.substring(index + 1), size - 1))));
             }
-
-            maxValue = String.valueOf(Math.max(Integer.valueOf(maxValue), Integer.valueOf(value)));
-
             index++;
         }
 
-        return maxValue;
+        return maxNumber;
     }
 }
 
